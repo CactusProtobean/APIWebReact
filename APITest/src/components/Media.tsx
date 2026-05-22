5d47d00f6dc2eb17751935663d6f7f3c5baebedd
Jaroslav Nesen <jara.nesen@gmail.com>
2026-05-22 14:07:49 +0200

    process of adding API key

--- APITest/src/components/Media.tsx
+++ APITest/src/components/Media.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react"
 import "./Media.css"
+
+
 const Media = () => {
+    const [trending, setTrending] = useState([]);
     return (
         <div className="media">
 
@@ -7,6 +11,10 @@ const Media = () => {
                 <div className="row-header">
                     <img src="/images/trending.svg" alt="Trending" />
                     <h1>Trending</h1>
+
+                </div>
+                <div className="trending-container">
+                    <p>Content</p>
                 </div>
             </div>
             <div className="row">
@@ -14,11 +22,17 @@ const Media = () => {
                     <img src="/images/artists.svg" alt="Artists" />
                     <h1>Artists</h1>
                 </div>
+                <div className="artists-container">
+                    <p>Content</p>
+                </div>
             </div>
             <div className="row">
                 <div className="row-header">
                     <img src="/images/clips.svg" alt="Clips" />
                     <h1>Clips</h1>
+                </div>
+                <div className="clips-container">
+                    <p>Content</p>
                 </div>
             </div>
             <div className="row">
@@ -26,8 +40,10 @@ const Media = () => {
                     <img src="/images/stories.svg" alt="Stories" />
                     <h1>Stories</h1>
                 </div>
+                <div className="stories-container">
+                    <p>Content</p>
+                </div>
             </div>
-
         </div>
     );
 };
