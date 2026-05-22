73b2a9720fc9fff6d0750faa75b3800393c8da27
Jaroslav Nesen <jara.nesen@gmail.com>
2026-05-22 11:38:09 +0200

    styled menu buttons

--- APITest/src/components/Header.tsx
+++ APITest/src/components/Header.tsx
@@ -1,16 +1,43 @@
+import { KeyboardArrowDown } from '@mui/icons-material';
 import './Header.css';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 
 const Header = () => {
     return (
     <div className="header">
         <img src="/images/giphyLogo.png" alt="logo" />
         <div className="menu">
-            <h2>Reactions</h2>
-            <h2>Entertainment</h2>
-            <h2>Sports</h2>
-            <h2>Stickers</h2>
-            <h2>Artists</h2>
-            <h2>MoreVertIcon</h2>
+            <div className="button-wrapper reactions">
+                <div className="menu-button">
+                    <h2>Reactions</h2>
+                </div>
+            </div>
+            <div className="button-wrapper entertainment">
+                <div className="menu-button">
+                    <h2>Entertainment</h2>
+                </div>
+            </div>
+            <div className="button-wrapper sports">
+                <div className="menu-button">
+                    <h2>Sports</h2>
+                </div>
+            </div>
+            <div className="button-wrapper stickers">
+                <div className="menu-button">
+                    <h2>Stickers</h2>
+                </div>
+            </div>
+            <div className="button-wrapper artists">
+                <div className="menu-button">
+                    <h2>Artists</h2>
+                </div>
+            </div>
+            <div className="button-wrapper moreverticon">
+                <div className="menu-button">
+                    <MoreVertIcon />
+                </div>
+            </div>
         </div>
         <div className="button">
             <h2>Upload</h2>
@@ -20,6 +47,8 @@ const Header = () => {
         </div>
         <div className="profile">
             <img src="/images/avatar.png" alt="avatar" />
+            <h2>Profile</h2>
+            <KeyboardArrowDown />
         </div>
     </div>
     );
